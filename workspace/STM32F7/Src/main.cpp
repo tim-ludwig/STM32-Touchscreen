7d92b33d445d18cf9bca6b43dc3fc9f175551b1b
Tim Ludwig <timcat.tl@gmail.com>
2021-11-26 23:15:31 +0100

    Game drawing

--- workspace/STM32F7/Src/main.cpp
+++ workspace/STM32F7/Src/main.cpp
@@ -15,27 +15,35 @@
 #include "Color.h"
 
 //*******************************************************************
+
+WORD min(WORD a, WORD b) {
+	 return a < b ? a : b;
+}
+
+void drawBoard() {
+	WORD sideLength = min(lcd.getWidth(), lcd.getHeight());
+
+	WORD cellSize = sideLength/3;
+	WORD x = sideLength < lcd.getWidth() ? (lcd.getWidth() - sideLength) / 2 : 0;
+	WORD y = sideLength < lcd.getHeight() ? (lcd.getHeight() - sideLength) / 2 : 0;
+
+	lcd.drawRectangle(0, 0, lcd.getWidth(), lcd.getHeight(), Color::White);
+	for(int i = 0; i <= 3; i++) {
+		lcd.drawLine(x, y + i * cellSize, x + sideLength, y + i * cellSize, 1, Color::Black);
+		lcd.drawLine(x + i * cellSize, y, x + i * cellSize, y + sideLength, 1, Color::Black);
+	}
+
+	lcd.refresh();
+}
+
 int main(void)
 {
   lcd.clear();
-  lcd.printf( 0, 0, 10, "Hello world!" );
-  lcd.refresh();
-  lcd.drawRectangle(   0, 0, 480, 480, Color::White     );
-  lcd.drawRectangle( 480, 0, 320, 480, Color::LightBlue );
-	
-  lcd.setZoom     ( 2 );
-  lcd.setBackColor( Color::Cyan );
-  lcd.setTextColor( Color::Black);
- 
-  lcd.refresh();
+  //lcd.setZoom(2);
 
-  int i = 0;
+  drawBoard();
 
   cHwI2Cmaster::Device i2cTouch( i2c, 0x54 /*I2C-Addresse*/ );
     
-  while(1)
-  {
-	lcd.drawText( 20, 50, 20, "i:%d",i++ );
-    lcd.refresh();
-  }
+
 }
