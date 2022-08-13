69a3e0e2396249e0a567abcd695ba18859d9e2e4
Tim Ludwig <timcat.tl@gmail.com>
2022-08-13 16:43:52 +0200

    Delete workspace/main directory

--- workspace/main/Lib/timer.h
+++
@@ -1,9 +0,0 @@
-//--------------------------------------------
-// Initialisiert den Timer 2
-// 
-// Die mit dem Funktionspointer angegebene 
-// Callback-Funktion wird zyklisch durch den 
-// Timer-Interrupt aufgerufen
-//--------------------------------------------
-void timerInit( unsigned cycleTime,  // Zykluszeit
-                void (*isr)(void) ); // Funktionspointer der Callback-Funktion
--- workspace/main/Src/Matrix.c
+++
@@ -1,8 +0,0 @@
-/*
- * Matrix.c
- *
- *  Created on: 22 Oct 2021
- *      Author: TIm Ludwig
- */
-
-
